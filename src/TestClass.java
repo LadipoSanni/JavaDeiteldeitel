2a0f0fdb32883c7ba333d8f8ee902122929f5989
Ladipo Sanni <ladicorp24m@gmail.com>
2023-03-22 17:53:50 +0100

    Interview Asseco

--- src/TestClass.java
+++ src/TestClass.java
@@ -16,5 +16,13 @@ public class TestClass {
         }
         scanner.close();
     }
+//    int[] table = {1,2,6,8,9};
+//
+//        for(int i = table.length-1;i >= 0; i--) {
+//        System.out.print(table[i] + " ");
+//    }
+//        System.out.println();
+//
+//}
 }
 
