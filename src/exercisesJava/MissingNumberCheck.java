b8b3597e538500d43c2132357c32e2d922742572
Ladipo Sanni <ladicorp24m@gmail.com>
2023-05-02 17:05:35 +0100

    missing number solved

--- src/exercisesJava/MissingNumberCheck.java
+++ src/exercisesJava/MissingNumberCheck.java
@@ -1,27 +1,39 @@
 package exercisesJava;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class MissingNumberCheck {
     public static void main(String[] args) {
         int[] numbers = {1,2,3,4,5,7,9};
+//        int[] numbers = {1,10,7,2,-22,8,3,9,4,15,7,17,-2,24,22};
+//        int[] numbers = {2,4,6,8,-14,18,12,14,16,20};
         checkMissingNumber(numbers);
     }
 
     private static void checkMissingNumber(int[] numbers) {
-        int previous =0;
-        int now = 0;
-        List<Integer> missingNumbers = new ArrayList<>();
-
-        for (int i = previous; i < numbers.length; i++) {
-            previous = numbers[i];
-            if (previous-now>=2){
-                now = numbers[i];
-                missingNumbers.add(now);
+        Arrays.sort(numbers);
+        int prev = 0;
+        int next = 0;
+        int numMissing = 0;
+        ArrayList<Integer> missingNumbers = new ArrayList<>();
+        ArrayList<Integer> tempCasing = new ArrayList<>();
+            for (int i = 0; i < numbers.length; i++) {
+                next = numbers[i];
+                tempCasing.add(next);
+                if (tempCasing.size()==2){
+                    prev = tempCasing.get(0);
+                    next = tempCasing.get(1);
+                    numMissing = next-prev;
+                    if (numMissing!=1&&next>1&&prev>1){
+                        while (prev<next-1){
+                        missingNumbers.add(++prev);
+                        }
+                    }
+                    prev = next;
+                    tempCasing.remove(tempCasing.get(0));
+                }
             }
-            System.out.println(previous);
-        }
-        System.out.println(missingNumbers + "is missing");
+        System.out.println("These are the List of missing numbers: "+missingNumbers);
     }
 }
