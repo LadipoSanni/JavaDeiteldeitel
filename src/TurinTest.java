b79304519083550d329143fddc9f1a0df643e363
Ladipo Sanni <ladicorp24m@gmail.com>
2023-05-16 12:54:07 +0100

    dataTypes

---
+++ src/TurinTest.java
@@ -0,0 +1,17 @@
+public class TurinTest {
+//    given a string S. when u iterate through that string S, the consecutive
+//    4 characters must follow each other in alphabetical order.
+//    while the iterator keep shifting 1 step till the end of the string with 4
+//    character at each shift.
+//    count how many shift has 4 characters in alphabetical order.
+public static void main(String[] args) {
+    String S = "abcdcfchijk";
+     shiftTheString(S);
+    }
+
+    private static int shiftTheString(String s) {
+    return 0;
+    }
+
+
+}
